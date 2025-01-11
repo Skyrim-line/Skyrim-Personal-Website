10efd9f355ed10e38c64da892d67208fb690bd2f
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2025-01-12 08:59:56 +1100

    modified homepage

--- skyrim/src/pages/components/navbar.jsx
+++ skyrim/src/pages/components/navbar.jsx
@@ -27,8 +27,9 @@ function Navbar() {
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="navbar bg-base-300 bg-opacity-75 w-full h-24 text-lg fixed top-0 z-50">
-          <div className="flex-none lg:hidden">
+        <div className="navbar bg-base-300 bg-opacity-100 w-full h-24 text-lg fixed top-0 z-50 px-4 flex items-center justify-between">
+          {/* 左侧 - 移动端菜单按钮 */}
+          <div className="lg:hidden flex-none">
             <label
               htmlFor="my-drawer-3"
               aria-label="open sidebar"
@@ -49,9 +50,10 @@ function Navbar() {
               </svg>
             </label>
           </div>
-          <div className="mx-2 flex-1 px-2">Navbar Title</div>
-          <div className="hidden flex-none lg:block">
-            <ul className="menu menu-horizontal text-base font-medium">
+          {/* 中间部分 - PC端 Logo 和导航 */}
+          <div className="hidden lg:flex flex-1 items-center justify-center space-x-10">
+            <div className="text-2xl font-bold">Skyrim Wu</div>
+            <ul className="menu menu-horizontal text-base font-medium space-x-10">
               <li>
                 <a>Home</a>
               </li>
@@ -66,16 +68,14 @@ function Navbar() {
               </li>
             </ul>
           </div>
-          {/* 主题切换按钮 */}
-          <div className="ml-2 mr-4 ">
+          {/* 右侧 - 主题切换按钮 */}
+          <div className="mx-2">
             <label className="swap swap-rotate">
-              {/* 切换按钮 */}
               <input
                 type="checkbox"
                 onChange={toggleTheme}
                 checked={theme === "dark"}
               />
-              {/* 太阳图标 */}
               <svg
                 className="swap-off h-10 w-10 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
@@ -83,7 +83,6 @@ function Navbar() {
               >
                 <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
               </svg>
-              {/* 月亮图标 */}
               <svg
                 className="swap-on h-10 w-10 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
@@ -94,8 +93,11 @@ function Navbar() {
             </label>
           </div>
         </div>
+
         {/* Page content here */}
       </div>
+
+      {/* Drawer - 移动端菜单 */}
       <div className="drawer-side">
         <label
           htmlFor="my-drawer-3"
@@ -103,7 +105,7 @@ function Navbar() {
           className="drawer-overlay"
         ></label>
         <ul className="menu bg-base-200 min-h-full w-80 p-7 flex flex-col items-center justify-start ">
-          <div className="text-2xl space-y-4 font-bold">
+          <div className="text-2xl space-y-6 font-bold">
             <li>
               <a>Home</a>
             </li>
--- skyrim/src/pages/home.jsx
+++ skyrim/src/pages/home.jsx
@@ -6,25 +6,48 @@ function Home() {
     <div className="bg-base-300">
       <Navbar />
       <main>
-        <div
-          className="hero min-h-screen"
+        {/* <div
+          className="hero min-h-screen "
           style={{
             backgroundImage: `url(${Skyrim})`,
           }}
         >
-          <div className="hero-overlay bg-opacity-60"></div>
-          <div className="hero-content text-neutral-content justify-end items-center flex">
+          <div className="hero bg-opacity-60"></div>
+          <div className=" hero-content text-neutral-content justify-end items-center flex">
             <div className="max-w-md">
               <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
               <p className="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-                excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id
-                nisi.
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
               </p>
               <button className="btn btn-primary">Get Started</button>
             </div>
           </div>
+        </div> */}
+        <div className="hero min-h-screen flex">
+          {/* 左侧部分 - 文字 */}
+          <div className="flex-1 flex items-center justify-center bg-gray-800 text-white p-8">
+            <div className="max-w-lg">
+              <h1 className="text-5xl font-bold mb-6">Welcome to Skyrim Wu</h1>
+              <p className="text-lg mb-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+
+          {/* 右侧部分 - 图片 */}
+          <div
+            className="flex-1 bg-cover bg-center"
+            style={{
+              backgroundImage: `url(${Skyrim})`,
+            }}
+          ></div>
         </div>
+
         <div className="hero bg-base-300 min-h-screen">
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img
