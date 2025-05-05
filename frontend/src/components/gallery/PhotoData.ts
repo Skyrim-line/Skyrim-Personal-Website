ae0f14299e25661c6c4ab285db571e14648d0f33
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2025-05-05 10:13:24 +1000

    Modifying gallery picture info 1.0

--- frontend/src/components/gallery/PhotoData.ts
+++ frontend/src/components/gallery/PhotoData.ts
@@ -2,7 +2,6 @@ import img1 from "./photos/1.jpg";
 import img2 from "./photos/2.jpg";
 import img3 from "./photos/3.jpg";
 import img4 from "./photos/4.jpg";
-import img5 from "./photos/5.jpg";
 import img6 from "./photos/6.jpg";
 import img7 from "./photos/7.jpg";
 import img8 from "./photos/8.jpeg";
@@ -17,28 +16,23 @@ export const photoData = [
   },
   {
     src: img2,
-    info: "Blue Mountains, 2023",
+    info: "Sydney Opera House",
     description:
       "The Blue Mountains is a mountainous region in New South Wales, Australia. It is known for its dramatic scenery and cultural significance.",
   },
   {
     src: img3,
-    info: "Bondi Beach, 2024",
+    info: "Bondi Beach",
     description:
       "Bondi Beach is a popular beach and the name of the surrounding suburb in Sydney. Bondi Beach is one of Australia's most famous beaches and is located 7 km east of the Sydney central business district.",
   },
   {
     src: img4,
-    info: "Melbourne Skyline, 2023",
+    info: "Palm Cove, Cairns",
     description:
       "Melbourne is the coastal capital of the southeastern Australian state of Victoria. It has a vibrant cultural scene and is known for its graffiti laneways, excellent restaurants, and major sports venues.",
   },
-  {
-    src: img5,
-    info: "Great Barrier Reef, 2023",
-    description:
-      "The Great Barrier Reef is the world's largest coral reef system, located in the Coral Sea off the coast of Queensland, Australia. It is one of the seven natural wonders of the world.",
-  },
+
   {
     src: img6,
     info: "Uluru, 2023",
