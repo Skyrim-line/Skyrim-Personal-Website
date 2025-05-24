26455c757070377ebd57a9b81949f876168da4f2
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2025-05-24 19:31:48 +1000

    Modify Vite config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": "/src", // ✅ 简单兼容 Vercel 的写法，路径从根目录起
     },
   },
 });
