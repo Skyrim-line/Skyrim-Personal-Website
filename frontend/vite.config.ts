b18f07466e4ee75d0523727eb34aba0cfe1acd1b
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2025-06-01 23:42:16 +1000

    Try to deploy it

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
+
 export default defineConfig({
   base: "/Skyrim-Personal-Website/", // 👈 注意仓库名大小写必须匹配
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
