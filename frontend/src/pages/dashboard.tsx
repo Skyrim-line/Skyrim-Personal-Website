58411c17ede14c8fd73061cd2179f465a3887a5b
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2025-06-01 23:46:31 +1000

    Try to deploy it 2.0

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/layout/Navbar";
+import Navbar from "@/components/layout/Navbar.tsx";
 import Skyrim from "../assets/Skyrim2.jpg";
 import { useState, useEffect } from "react";
 import PhotoGallery from "../components/gallery/PhotoGallery";
